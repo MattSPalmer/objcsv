f0727423ae95adc47459ebcd717aaf5601bb71bf
Matt Palmer <palmer.s.matt@gmail.com>
2012-11-04 01:41:01 -0400

    Initial commit.

---
+++ csv.go
@@ -0,0 +1,162 @@
+package csv
+
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"reflect"
+	"strconv"
+)
+
+func csvError(msg string, args ...interface{}) error {
+	return fmt.Errorf("CSV: "+msg, args...)
+}
+
+// ReadCSV reads from reader r into a pointer to a slice of the destination struct type.
+func ReadCSV(r io.Reader, ptrToSlice interface{}) (error) {
+  pv := reflect.ValueOf(ptrToSlice)
+	if pv.IsNil() {
+		return csvError("expected %v to have a non-nil value", ptrToSlice)
+	}
+	if pv.Kind() != reflect.Ptr || pv.Elem().Kind() != reflect.Slice {
+		return csvError("expected a pointer to a slice, got %v instead", pv.Kind())
+	}
+
+	t := pv.Elem().Type().Elem()
+	cr := csv.NewReader(r)
+	didHeader := false
+	for {
+		record, err := cr.Read()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return err
+		}
+		if !didHeader {
+			didHeader = true
+			continue
+		}
+
+		rv, err := recordToValue(record, t)
+		if err != nil {
+			return err
+		}
+    pv.Elem().Set(reflect.Append(pv.Elem(), rv.Elem()))
+	}
+	return nil
+}
+
+func recordToValue(ss []string, t reflect.Type) (reflect.Value, error) {
+	v := reflect.New(t)
+	if len(ss) != t.NumField() {
+		return v, csvError("recordToValue: can't decode CSV record to struct (field mismatch)")
+	}
+	for i, s := range ss {
+    if s == "" {
+      continue
+    }
+		f := t.Field(i)
+		ft := f.Type.Kind()
+		fv := v.Elem().Field(i)
+		typeError := func(msg string, e error) error {
+      return csvError("%v: type %v, field %v: %v", msg, ft, f.Name, e)
+		}
+
+		switch ft {
+		case reflect.String:
+			fv.SetString(s)
+    case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+			theInt, err := strconv.ParseInt(s, 10, 64)
+			if err != nil {
+				return v, typeError("int", err)
+			}
+			fv.SetInt(theInt)
+		case reflect.Float64:
+			theFloat, err := strconv.ParseFloat(s, 64)
+			if err != nil {
+				return v, typeError("float64", err)
+			}
+			fv.SetFloat(theFloat)
+		case reflect.Bool:
+			theBool, err := strconv.ParseBool(s)
+			if err != nil {
+				return v, typeError("bool", err)
+			}
+			fv.SetBool(theBool)
+		default:
+			return v, csvError("recordToValue: unsupported type %v in field %v", ft, f.Name)
+		}
+
+	}
+	return v, nil
+}
+
+// WriteCSV writes to writer w from a slice of data records.
+func WriteCSV(w io.Writer, slice interface{}) error {
+	// ensure that d is a slice containing at least some data.
+	v, err := reflectSlice(slice)
+	if err != nil {
+		return err
+	}
+	if v.Len() == 0 {
+		return csvError("data is empty")
+	}
+
+	cw := csv.NewWriter(w)
+	if err := writeHeader(cw, v.Index(0).Interface()); err != nil {
+		return err
+	}
+	for i := 0; i < v.Len(); i++ {
+		e := v.Index(i)
+		if err := writeRow(cw, e.Interface()); err != nil {
+			return err
+		}
+	}
+	cw.Flush()
+	return nil
+}
+
+func writeHeader(w *csv.Writer, d interface{}) error {
+	v := reflect.ValueOf(d)
+	header := make([]string, v.NumField())
+	for i := 0; i < v.NumField(); i++ {
+		header[i] = v.Type().Field(i).Name
+	}
+	if err := w.Write(header); err != nil {
+		return err
+	}
+	return nil
+}
+
+func writeRow(w *csv.Writer, d interface{}) error {
+	v := reflect.ValueOf(d)
+	row := make([]string, v.NumField())
+	for i := 0; i < v.NumField(); i++ {
+		switch t := v.Field(i).Interface(); t.(type) {
+		case int:
+			row[i] = fmt.Sprintf("%d", t)
+		case float64:
+			row[i] = fmt.Sprintf("%.2f", t)
+		case string:
+			row[i] = fmt.Sprintf("%s", t)
+		default:
+			row[i] = fmt.Sprintf("%v", t)
+		}
+	}
+	if err := w.Write(row); err != nil {
+		return err
+	}
+	return nil
+}
+
+func reflectSlice(i interface{}) (reflect.Value, error) {
+	v := reflect.ValueOf(i)
+	if v.IsNil() {
+		return v, csvError("expected %v to have a non-nil value", i)
+	}
+	if v.Kind() != reflect.Slice {
+		return v, csvError("expected a slice, got %v instead", v.Kind())
+	}
+	return v, nil
+}
---
+++ csv_test.go
@@ -0,0 +1,120 @@
+package csv
+
+import (
+	"bytes"
+	"encoding/csv"
+	"io"
+	"reflect"
+	"testing"
+)
+
+type dumbStruct struct {
+	FName string
+	LName string
+	Age   int
+	Worth float64
+}
+
+func TestWriteHeader(t *testing.T) {
+	d := dumbStruct{"Hello", "World", 42, 10.01}
+	buf := new(bytes.Buffer)
+	cw := csv.NewWriter(buf)
+	err := writeHeader(cw, d)
+
+	if err != nil {
+		t.Fatalf("Unexpected error from writeHeader: %v", err)
+	}
+
+	cw.Flush()
+	actual := buf.String()
+	expected := "FName,LName,Age,Worth\n"
+	if actual != expected {
+		t.Errorf("Header is wrong, expected %q, got %q", expected, actual)
+	}
+}
+
+func TestWriteRow(t *testing.T) {
+	d := dumbStruct{"Matt", "Palmer", 23, 40000.00}
+	buf := new(bytes.Buffer)
+	cw := csv.NewWriter(buf)
+	err := writeRow(cw, d)
+
+	if err != nil {
+		t.Fatalf("Unexpected error from writeRow: %v", err)
+	}
+
+	cw.Flush()
+	actual := buf.String()
+	expected := "Matt,Palmer,23,40000.00\n"
+	if actual != expected {
+		t.Errorf("Row is wrong, expected %q, got %q", expected, actual)
+	}
+}
+
+func TestReadCSV(t *testing.T) {
+	data := `FName,LName,Age,Worth
+Matt,Palmer,23,40000.00
+Robert,Sesek,23,60000.00
+Barack,Obama,51,80000000.00
+Mitt,Romney,59,9950000000.00`
+
+	buf := new(bytes.Buffer)
+
+	_, err := buf.WriteString(data)
+	if err != nil {
+		t.Fatalf("Error in testing data: %v", err)
+	}
+
+	slice := make([]dumbStruct, 0)
+	err = ReadCSV(buf, &slice)
+	if err != nil {
+		t.Fatalf("Unexpected error from ReadCSV: %v", err)
+	}
+
+	expected := []dumbStruct{
+		{"Matt", "Palmer", 23, 40000.00},
+		{"Robert", "Sesek", 23, 60000.00},
+		{"Barack", "Obama", 51, 80000000.00},
+		{"Mitt", "Romney", 59, 9950000000.00},
+	}
+
+	if !reflect.DeepEqual(slice, expected) {
+		t.Errorf("Expected %v but got %v", expected, slice)
+	}
+
+}
+
+func TestWriteCSV(t *testing.T) {
+	d := []dumbStruct{
+		{"Matt", "Palmer", 23, 40000.00},
+		{"Robert", "Sesek", 23, 60000.00},
+		{"Barack", "Obama", 51, 80000000.00},
+		{"Mitt", "Romney", 59, 9950000000.00},
+	}
+	buf := new(bytes.Buffer)
+
+	if err := WriteCSV(buf, d); err != nil {
+		t.Fatalf("Unexpected error from WriteCSV: %v", err)
+	}
+
+	expected := []string{
+		"FName,LName,Age,Worth\n",
+		"Matt,Palmer,23,40000.00\n",
+		"Robert,Sesek,23,60000.00\n",
+		"Barack,Obama,51,80000000.00\n",
+		"Mitt,Romney,59,9950000000.00\n",
+	}
+	for i := 0; i < 5; i++ {
+		actual, err := buf.ReadString('\n')
+		if err != nil {
+			t.Fatalf("Unexpected output at line %d: %v", i, err)
+		}
+		if actual != expected[i] {
+			t.Errorf("Row %d is wrong, expected %q, got %q", i, expected[i], actual)
+		}
+	}
+	s, err := buf.ReadString('\n')
+	if s != "" || err != io.EOF {
+		t.Errorf("Got unexpected data %q where expected EOF", s)
+	}
+}
